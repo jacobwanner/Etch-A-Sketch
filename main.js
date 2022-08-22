5b64df3fbe0b291f92fb651f44fc660d4e54b103
Jacob Wanner <106052324+jacobwanner@users.noreply.github.com>
2022-08-21 22:42:33 -0400

    Added eventlisteners and color change function
    I replaced my original eventListener as I knew I could use an if, else function the right way. I learned the eventListener needs to be added to the for loop in order for all of the divs to be watching for the event.

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const container = document.getElementById('container');
-for (let x=0; x<9; x++) {
+for (let x=0; x<256; x++) {
     const square = document.createElement('div');
-    square.className = 'square';
+    square.className = 'square empty-square';
+    square.addEventListener('mouseover', changeColor);
+    square.addEventListener('mouseout', changeColor);
     container.appendChild(square);
 }
 
-const squareDiv = document.getElementById('square');
-squareDiv.onmouseover = function() {
-    this.style.backgroundColor = 'grey';
-};
-squareDiv.onmouseout = function () {
-    this.style.backgroundColor = 'black';
-};
+const square = document.getElementsByClassName('square');
+
+function changeColor(e) {
+    if (e.type === 'mouseover') {
+        e.target.style.backgroundColor = 'grey';
+    } else if (e.type === 'mouseout') {
+        e.target.style.backgroundColor = 'black';
+    }
+}
