c7b5666f89cfba21a0ba0d8cee24a0b42d87a8c4
Jacob Wanner <106052324+jacobwanner@users.noreply.github.com>
2022-08-25 14:57:54 -0400

    Grid size is now proportionate and makes a square
    -I implemented a repeat() function into the container style to set up the columns and rows according to the input given by the user.
    -I added the grid setup loop into a function so the user could press the button on screen to give a new size of grid they want and then the code carries out the loop of making all the squares and placing them in a grid format.

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const container = document.getElementById('container');
-for (let x=0; x<256; x++) {
-    const square = document.createElement('div');
-    square.className = 'square empty-square';
-    square.addEventListener('mouseover', changeColor);
-    square.addEventListener('mouseout', changeColor);
-    container.appendChild(square);
-}
-
 const square = document.getElementsByClassName('square');
+const newGridBtn = document.getElementById('setupGrid');
+newGridBtn.addEventListener('click', newGrid);
+
+function newGrid() {
+    let gridSize = prompt("How many squares per side would you like?");
+
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const square = document.createElement('div');
+        square.className = 'square empty-square';
+        square.addEventListener('mouseover', changeColor);
+        square.addEventListener('mouseout', changeColor);
+        container.appendChild(square);
+    }
+}
 
 function changeColor(e) {
     if (e.type === 'mouseover') {
